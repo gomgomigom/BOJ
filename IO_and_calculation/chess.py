2051b915d03662d560ea3f93611902366addee5a
gomgomigom <nodam89@naver.com>
2022-12-30 11:07:47 +0900

    if

---
+++ IO_and_calculation/chess.py
@@ -0,0 +1,56 @@
+# a = input()
+# print(a + "??!")
+# 2541
+# 1998
+# b = input()
+# print(int(b) - 543)
+
+# a = input().split()
+# b = [1, 1, 2, 2, 2, 8]
+# result = []
+# for i, item in enumerate(a, start=0):
+#     result.append(str(b[i] - int(item)))
+# print(" ".join(result)472
+385
+
+# a = list(map(int, input().split()))
+# A, B, C = a
+# print((A + B) % C)
+# print(((A % C) + (B % C)) % C)
+# print((A * B) % C)
+# print(((A % C) * (B % C)) % C)
+
+# A = input()
+# B = input()
+# for i, b in enumerate(B, start=1):
+#     print(int(A) * int(B[-i]))
+# print(int(A) * int(B))
+
+# 472
+# 385
+
+
+# print(
+#     """\    /\\
+#  )  ( ')
+# (  /  )
+#  \(__)|
+#     """
+# )
+
+# print(
+#     '''|\_/|
+# |q p|   /}
+# ( 0 )"""\\
+# |"^"`    |
+# ||_/=\\\\__|'''
+# )
+
+# print(
+#     """         ,r'"7
+# r`-_   ,'  ,/
+#  \. ". L_r'
+#    `~\/
+#       |
+#       |"""
+# )
---
+++ if/big_small.py
@@ -0,0 +1,67 @@
+# a = list(map(int, input().split()))
+# A, B = a
+# if A > B:
+#     print(">")
+# elif B > A:
+#     print("<")
+# else:
+#     print("==")
+
+# a = int(input())
+# if a >= 90:
+#     print("A")
+# elif a >= 80:
+#     print("B")
+# elif a >= 70:
+#     print("C")
+# elif a >= 60:
+#     print("D")
+# else:
+#     print("F")
+
+# a = int(input())
+# if a % 4 == 0 and (a % 100 != 0 or a % 400 == 0):
+#     print(1)
+# else:
+#     print(0)
+
+# a = int(input())
+# b = int(input())
+# result = ""
+# if a > 0 and b > 0:
+#     print(1)
+# elif a > 0 and b < 0:
+#     print(4)
+# elif a < 0 and b > 0:
+#     print(2)
+# else:
+#     print(3)
+
+# a = list(map(int, input().split()))
+# x, y = a
+
+# if y - 45 < 0:
+#     x -= 1
+#     y = 60 - 45 + y
+#     if x < 0:
+#         x = 23
+# else:
+#     y = y - 45
+# print(x, y)
+
+a = list(map(int, input().split()))
+t = int(input())
+h, m = a
+if t >= 60:
+    h += t // 60
+    t = t % 60
+    if h >= 24:
+        h = h % 24
+if m + t >= 60:
+    h += 1
+    m = m + t - 60
+    if h == 24:
+        h = 0
+else:
+    m = m + t
+print("%d %d" % (h, m))
