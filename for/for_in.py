a2a78d017e7a6d562d0c20eae847638936614608
gomgomigom <nodam89@naver.com>
2023-01-01 20:01:19 +0900

    readline

--- for/for_in.py
+++ for/for_in.py
@@ -1,3 +1,14 @@
-input = int(input())
-for i in range(1, 10):
-    print(f"{input} * {i} = {input * i}")
+# input = int(input())
+# for i in range(1, 10):
+#     print(f"{input} * {i} = {input * i}")
+
+import sys
+
+len_num = int(sys.stdin.readline())
+num_list = [
+    list(map(int, sys.stdin.readline().split())) for i in range(len_num)
+]
+for i in num_list:
+    print(i[0] + i[1])
+
+a = 5
